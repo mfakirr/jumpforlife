df6a16777a68ff101e985f3ebe063f437c1091b8
mfakir <46379342+mfakirr@users.noreply.github.com>
2020-12-28 18:20:25 +0300

    Add files via upload

---
+++ TamEkran.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TamEkran : MonoBehaviour
+{
+  
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 tempScale = transform.localScale;
+
+        float spriteGenislik = spriteRenderer.size.x;
+        float ekranYukseklik = Camera.main.orthographicSize * 2.0f;
+        float ekranGenislik = ekranYukseklik / Screen.height * Screen.width;
+        tempScale.x = ekranGenislik / spriteGenislik;
+        transform.localScale = tempScale;
+    }
+
+
+}
